869832725e078e51039f967a00bb9316749e2f22
Vladyslav Zaiets <rutova2@gmail.com>
2025-07-31 20:07:12 +0300

    more AggregateRoot extension methods

--- Domain/AggregateRoots/AggregateRootExtensions.cs
+++ Domain/AggregateRoots/AggregateRootExtensions.cs
@@ -20,6 +20,7 @@ public static class AggregateRootExtensions
     /// Determines whether this aggregate has any uncommitted events.
     /// </summary>
     /// <param name="aggregate">The aggregate root instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="aggregate"/> is <see langword="null"/>.</exception>
     /// <returns><see langword="true"/> if there are uncommitted events; otherwise, <see langword="false"/>.</returns>
     public static bool HasUncommittedEvents(this AggregateRoot aggregate)
     {
@@ -31,6 +32,7 @@ public static class AggregateRootExtensions
     /// Gets the number of uncommitted events for this aggregate.
     /// </summary>
     /// <param name="aggregate">The aggregate root instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="aggregate"/> is <see langword="null"/>.</exception>
     /// <returns>The count of uncommitted events.</returns>
     public static int UncommittedEventsCount(this AggregateRoot aggregate)
     {
@@ -42,6 +44,7 @@ public static class AggregateRootExtensions
     /// Determines whether this aggregate has been modified since it was created.
     /// </summary>
     /// <param name="aggregate">The aggregate root instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="aggregate"/> is <see langword="null"/>.</exception>
     /// <returns><see langword="true"/> if the aggregate has been modified (Version > 0); otherwise, <see langword="false"/>.</returns>
     public static bool IsModified(this AggregateRoot aggregate)
     {
@@ -53,6 +56,7 @@ public static class AggregateRootExtensions
     /// Gets the age of the aggregate in UTC.
     /// </summary>
     /// <param name="aggregate">The aggregate root instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="aggregate"/> is <see langword="null"/>.</exception>
     /// <returns>A <see cref="TimeSpan"/> representing how long the aggregate has existed.</returns>
     public static TimeSpan GetAge(this AggregateRoot aggregate)
     {
