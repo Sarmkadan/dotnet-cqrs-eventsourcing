96f474072d61be73ea4b6e3eeac6edd1aa1edbfa
Vladyslav Zaiets <rutova2@gmail.com>
2025-08-11 14:01:21 +0300

    feat: a couple of ErrorHandlingMiddleware extensions

--- Infrastructure/Middleware/ErrorHandlingMiddlewareExtensions.cs
+++ Infrastructure/Middleware/ErrorHandlingMiddlewareExtensions.cs
@@ -3,17 +3,22 @@ using Microsoft.AspNetCore.Http;
 namespace Infrastructure.Middleware
 {
     /// <summary>
-    /// Extension methods for configuring and using ErrorHandlingMiddleware in the request pipeline.
+    /// Provides extension methods for integrating global error handling middleware into the ASP.NET request pipeline.
+    /// These extensions ensure that unhandled exceptions are captured early in the pipeline.
     /// </summary>
     public static class ErrorHandlingMiddlewareExtensions
     {
         /// <summary>
-        /// Adds global error handling middleware to the request pipeline.
-        /// This should be registered as the first middleware to catch all exceptions.
+        /// Adds the <see cref="ErrorHandlingMiddleware"/> to the request pipeline as the first middleware.
+        /// This ensures that all unhandled exceptions are captured before being processed by downstream middleware.
         /// </summary>
-        /// <param name="builder">The application builder</param>
-        /// <returns>The configured application builder</returns>
-        /// <exception cref="ArgumentNullException">Thrown if builder is null</exception>
+        /// <param name="builder">The application builder used to configure the middleware pipeline.</param>
+        /// <returns>
+        /// The same <see cref="IApplicationBuilder"/> instance to allow for fluent configuration.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="builder"/> is <see langword="null"/>, as the builder is required to configure the pipeline.
+        /// </exception>
         public static IApplicationBuilder UseGlobalErrorHandling(this IApplicationBuilder builder)
         {
             ArgumentNullException.ThrowIfNull(builder);
