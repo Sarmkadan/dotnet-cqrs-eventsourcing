05e64fdfd82bfb81e847522c1874fe646eaf9634
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-27 15:14:27 +0300

    chore: update Sagas

---
+++ Application/Sagas.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Saga
+{
+    public class Saga
+    {
+        public Saga()
+        {
+        }
+
+        public void RequestTimeout(string sagaId, int delay, string message)
+        {
+            // TO DO: implement timeout logic
+        }
+    }
+}
