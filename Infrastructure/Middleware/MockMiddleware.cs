806aa56838e108b7491066c0f12e8a8aac404297
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-10 14:05:00 +0000

    chore: apply workspace updates

--- Infrastructure/Middleware/MockMiddleware.cs
+++ Infrastructure/Middleware/MockMiddleware.cs
@@ -8,6 +8,13 @@ namespace DotNetCqrsEventSourcing.Infrastructure.Middleware
     {
         public async Task<HttpResponseMessage> InvokeAsync(HttpRequestMessage request, Func<HttpRequestMessage, Func<CancellationToken, Task<HttpResponseMessage>>, Task<HttpResponseMessage>> next, CancellationToken cancellationToken)
         {
+            if (request == null)
+                throw new ArgumentNullException(nameof(request));
+            if (next == null)
+                throw new ArgumentNullException(nameof(next));
+            return await next(request, async (req, ct) => await Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)));
+        }
+        {
             return await next(request, async (req, ct) => await Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)));
             // TODO: implement idempotency middleware
             // TODO: implement idempotency middleware        }
