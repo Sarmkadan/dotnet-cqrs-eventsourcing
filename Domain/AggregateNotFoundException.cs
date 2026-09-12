4b8398ca8992ef5a4d85a4d73a84f9e0c31728d8
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-12 08:43:55 +0000

    chore: apply codex edits

--- Domain/AggregateNotFoundException.cs
+++ Domain/AggregateNotFoundException.cs
@@ -3,9 +3,16 @@ using System.Runtime.Serialization;
 
 namespace Domain
 {
+    /// <summary>
+    /// Represents an error that occurs when a requested aggregate cannot be found.
+    /// </summary>
     [Serializable]
     public class AggregateNotFoundException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateNotFoundException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public AggregateNotFoundException(string message) : base(message)
         {
         }
